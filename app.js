ee9aced70912d16c0d3add937ae10b167bcc789f
FourSeagulls <114670998+FourSeagulls@users.noreply.github.com>
2023-07-07 23:47:29 +0200

    Tarjetas y botones app.js
    Aquí está la lógica de los botones de las tarjetas.

---
+++ app.js
@@ -0,0 +1,387 @@
+const open_1 = document.getElementById('open_1');
+const tarjetaContenedor_1 = document.getElementById('tarjeta-contenedor_1');
+const close_1 = document.getElementById('close_1');
+
+open_1.addEventListener('click', () => {
+    tarjetaContenedor_1.classList.add('show');
+});
+
+close_1.addEventListener('click', () => {
+    tarjetaContenedor_1.classList.remove('show');
+});
+
+
+const open_2 = document.getElementById('open_2');
+const tarjetaContenedor_2 = document.getElementById('tarjeta-contenedor_2');
+const close_2 = document.getElementById('close_2');
+
+open_2.addEventListener('click', () => {
+    tarjetaContenedor_2.classList.add('show');
+});
+
+close_2.addEventListener('click', () => {
+    tarjetaContenedor_2.classList.remove('show');
+});
+
+
+const open_3 = document.getElementById('open_3');
+const tarjetaContenedor_3 = document.getElementById('tarjeta-contenedor_3');
+const close_3 = document.getElementById('close_3');
+
+open_3.addEventListener('click', () => {
+    tarjetaContenedor_3.classList.add('show');
+});
+
+close_3.addEventListener('click', () => {
+    tarjetaContenedor_3.classList.remove('show');
+});
+
+
+const open_4 = document.getElementById('open_4');
+const tarjetaContenedor_4 = document.getElementById('tarjeta-contenedor_4');
+const close_4 = document.getElementById('close_4');
+
+open_4.addEventListener('click', () => {
+    tarjetaContenedor_4.classList.add('show');
+});
+
+close_4.addEventListener('click', () => {
+    tarjetaContenedor_4.classList.remove('show');
+});
+
+
+const open_5 = document.getElementById('open_5');
+const tarjetaContenedor_5 = document.getElementById('tarjeta-contenedor_5');
+const close_5 = document.getElementById('close_5');
+
+open_5.addEventListener('click', () => {
+    tarjetaContenedor_5.classList.add('show');
+});
+
+close_5.addEventListener('click', () => {
+    tarjetaContenedor_5.classList.remove('show');
+});
+
+
+const open_6 = document.getElementById('open_6');
+const tarjetaContenedor_6 = document.getElementById('tarjeta-contenedor_6');
+const close_6 = document.getElementById('close_6');
+
+open_6.addEventListener('click', () => {
+    tarjetaContenedor_6.classList.add('show');
+});
+
+close_6.addEventListener('click', () => {
+    tarjetaContenedor_6.classList.remove('show');
+});
+
+
+const open_7 = document.getElementById('open_7');
+const tarjetaContenedor_7 = document.getElementById('tarjeta-contenedor_7');
+const close_7 = document.getElementById('close_7');
+
+open_7.addEventListener('click', () => {
+    tarjetaContenedor_7.classList.add('show');
+});
+
+close_7.addEventListener('click', () => {
+    tarjetaContenedor_7.classList.remove('show');
+});
+
+
+const open_8 = document.getElementById('open_8');
+const tarjetaContenedor_8 = document.getElementById('tarjeta-contenedor_8');
+const close_8 = document.getElementById('close_8');
+
+open_8.addEventListener('click', () => {
+    tarjetaContenedor_8.classList.add('show');
+});
+
+close_8.addEventListener('click', () => {
+    tarjetaContenedor_8.classList.remove('show');
+});
+
+
+const open_9 = document.getElementById('open_9');
+const tarjetaContenedor_9 = document.getElementById('tarjeta-contenedor_9');
+const close_9 = document.getElementById('close_9');
+
+open_9.addEventListener('click', () => {
+    tarjetaContenedor_9.classList.add('show');
+});
+
+close_9.addEventListener('click', () => {
+    tarjetaContenedor_9.classList.remove('show');
+});
+
+
+const open_10 = document.getElementById('open_10');
+const tarjetaContenedor_10 = document.getElementById('tarjeta-contenedor_10');
+const close_10 = document.getElementById('close_10');
+
+open_10.addEventListener('click', () => {
+    tarjetaContenedor_10.classList.add('show');
+});
+
+close_10.addEventListener('click', () => {
+    tarjetaContenedor_10.classList.remove('show');
+});
+
+
+const open_11 = document.getElementById('open_11');
+const tarjetaContenedor_11 = document.getElementById('tarjeta-contenedor_11');
+const close_11 = document.getElementById('close_11');
+
+open_11.addEventListener('click', () => {
+    tarjetaContenedor_11.classList.add('show');
+});
+
+close_11.addEventListener('click', () => {
+    tarjetaContenedor_11.classList.remove('show');
+});
+
+
+const open_12 = document.getElementById('open_12');
+const tarjetaContenedor_12 = document.getElementById('tarjeta-contenedor_12');
+const close_12 = document.getElementById('close_12');
+
+open_12.addEventListener('click', () => {
+    tarjetaContenedor_12.classList.add('show');
+});
+
+close_12.addEventListener('click', () => {
+    tarjetaContenedor_12.classList.remove('show');
+});
+
+
+const open_13 = document.getElementById('open_13');
+const tarjetaContenedor_13 = document.getElementById('tarjeta-contenedor_13');
+const close_13 = document.getElementById('close_13');
+
+open_13.addEventListener('click', () => {
+    tarjetaContenedor_13.classList.add('show');
+});
+
+close_13.addEventListener('click', () => {
+    tarjetaContenedor_13.classList.remove('show');
+});
+
+
+const open_14 = document.getElementById('open_14');
+const tarjetaContenedor_14 = document.getElementById('tarjeta-contenedor_14');
+const close_14 = document.getElementById('close_14');
+
+open_14.addEventListener('click', () => {
+    tarjetaContenedor_14.classList.add('show');
+});
+
+close_14.addEventListener('click', () => {
+    tarjetaContenedor_14.classList.remove('show');
+});
+
+
+const open_15 = document.getElementById('open_15');
+const tarjetaContenedor_15 = document.getElementById('tarjeta-contenedor_15');
+const close_15 = document.getElementById('close_15');
+
+open_15.addEventListener('click', () => {
+    tarjetaContenedor_15.classList.add('show');
+});
+
+close_15.addEventListener('click', () => {
+    tarjetaContenedor_15.classList.remove('show');
+});
+
+
+const open_16 = document.getElementById('open_16');
+const tarjetaContenedor_16 = document.getElementById('tarjeta-contenedor_16');
+const close_16 = document.getElementById('close_16');
+
+open_16.addEventListener('click', () => {
+    tarjetaContenedor_16.classList.add('show');
+});
+
+close_16.addEventListener('click', () => {
+    tarjetaContenedor_16.classList.remove('show');
+});
+
+
+const open_17 = document.getElementById('open_17');
+const tarjetaContenedor_17 = document.getElementById('tarjeta-contenedor_17');
+const close_17 = document.getElementById('close_17');
+
+open_17.addEventListener('click', () => {
+    tarjetaContenedor_17.classList.add('show');
+});
+
+close_17.addEventListener('click', () => {
+    tarjetaContenedor_17.classList.remove('show');
+});
+
+
+const open_18 = document.getElementById('open_18');
+const tarjetaContenedor_18 = document.getElementById('tarjeta-contenedor_18');
+const close_18 = document.getElementById('close_18');
+
+open_18.addEventListener('click', () => {
+    tarjetaContenedor_18.classList.add('show');
+});
+
+close_18.addEventListener('click', () => {
+    tarjetaContenedor_18.classList.remove('show');
+});
+
+
+const open_19 = document.getElementById('open_19');
+const tarjetaContenedor_19 = document.getElementById('tarjeta-contenedor_19');
+const close_19 = document.getElementById('close_19');
+
+open_19.addEventListener('click', () => {
+    tarjetaContenedor_19.classList.add('show');
+});
+
+close_19.addEventListener('click', () => {
+    tarjetaContenedor_19.classList.remove('show');
+});
+
+
+const open_20 = document.getElementById('open_20');
+const tarjetaContenedor_20 = document.getElementById('tarjeta-contenedor_20');
+const close_20 = document.getElementById('close_20');
+
+open_20.addEventListener('click', () => {
+    tarjetaContenedor_20.classList.add('show');
+});
+
+close_20.addEventListener('click', () => {
+    tarjetaContenedor_20.classList.remove('show');
+});
+
+
+const open_21 = document.getElementById('open_21');
+const tarjetaContenedor_21 = document.getElementById('tarjeta-contenedor_21');
+const close_21 = document.getElementById('close_21');
+
+open_21.addEventListener('click', () => {
+    tarjetaContenedor_21.classList.add('show');
+});
+
+close_21.addEventListener('click', () => {
+    tarjetaContenedor_21.classList.remove('show');
+});
+
+
+const open_22 = document.getElementById('open_22');
+const tarjetaContenedor_22 = document.getElementById('tarjeta-contenedor_22');
+const close_22 = document.getElementById('close_22');
+
+open_22.addEventListener('click', () => {
+    tarjetaContenedor_22.classList.add('show');
+});
+
+close_22.addEventListener('click', () => {
+    tarjetaContenedor_22.classList.remove('show');
+});
+
+const open_23 = document.getElementById('open_23');
+const tarjetaContenedor_23 = document.getElementById('tarjeta-contenedor_23');
+const close_23 = document.getElementById('close_23');
+
+open_23.addEventListener('click', () => {
+    tarjetaContenedor_23.classList.add('show');
+});
+
+close_23.addEventListener('click', () => {
+    tarjetaContenedor_23.classList.remove('show');
+});
+
+
+const open_24 = document.getElementById('open_24');
+const tarjetaContenedor_24 = document.getElementById('tarjeta-contenedor_24');
+const close_24 = document.getElementById('close_24');
+
+open_24.addEventListener('click', () => {
+    tarjetaContenedor_24.classList.add('show');
+});
+
+close_24.addEventListener('click', () => {
+    tarjetaContenedor_24.classList.remove('show');
+});
+
+
+const open_25 = document.getElementById('open_25');
+const tarjetaContenedor_25 = document.getElementById('tarjeta-contenedor_25');
+const close_25 = document.getElementById('close_25');
+
+open_25.addEventListener('click', () => {
+    tarjetaContenedor_25.classList.add('show');
+});
+
+close_25.addEventListener('click', () => {
+    tarjetaContenedor_25.classList.remove('show');
+});
+
+
+const open_26 = document.getElementById('open_26');
+const tarjetaContenedor_26 = document.getElementById('tarjeta-contenedor_26');
+const close_26 = document.getElementById('close_26');
+
+open_26.addEventListener('click', () => {
+    tarjetaContenedor_26.classList.add('show');
+});
+
+close_26.addEventListener('click', () => {
+    tarjetaContenedor_26.classList.remove('show');
+});
+
+
+const open_27 = document.getElementById('open_27');
+const tarjetaContenedor_27 = document.getElementById('tarjeta-contenedor_27');
+const close_27 = document.getElementById('close_27');
+
+open_27.addEventListener('click', () => {
+    tarjetaContenedor_27.classList.add('show');
+});
+
+close_27.addEventListener('click', () => {
+    tarjetaContenedor_27.classList.remove('show');
+});
+
+
+const open_28 = document.getElementById('open_28');
+const tarjetaContenedor_28 = document.getElementById('tarjeta-contenedor_28');
+const close_28 = document.getElementById('close_28');
+
+open_28.addEventListener('click', () => {
+    tarjetaContenedor_28.classList.add('show');
+});
+
+close_28.addEventListener('click', () => {
+    tarjetaContenedor_28.classList.remove('show');
+});
+
+
+const open_29 = document.getElementById('open_29');
+const tarjetaContenedor_29 = document.getElementById('tarjeta-contenedor_29');
+const close_29 = document.getElementById('close_29');
+
+open_29.addEventListener('click', () => {
+    tarjetaContenedor_29.classList.add('show');
+});
+
+close_29.addEventListener('click', () => {
+    tarjetaContenedor_29.classList.remove('show');
+});
+
+
+const open_30 = document.getElementById('open_30');
+const tarjetaContenedor_30 = document.getElementById('tarjeta-contenedor_30');
+const close_30 = document.getElementById('close_30');
+
+open_30.addEventListener('click', () => {
+    tarjetaContenedor_30.classList.add('show');
+});
+
+close_30.addEventListener('click', () => {
+    tarjetaContenedor_30.classList.remove('show');
+});
